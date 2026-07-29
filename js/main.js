a0a4607f18118436f55b243ee59e39ba8c19c506
兰政鹏 <lanzhengpeng@192.168.1.2>
2026-07-30 07:11:26 +0800

    Disable SPA router and use classic full-page navigation; fix anchor scroll bounce

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ import { initTheme } from './modules/theme.js';
 import './modules/background.js';
 import './modules/boids.js';
 import { initAudio } from './modules/audio.js';
-import { initRouter } from './modules/router.js';
+import { init as initHome } from './modules/home.js';
 
 initTheme();
 initAudio();
-initRouter();
+initHome();
--- works/js/main.js
+++ works/js/main.js
@@ -2,8 +2,6 @@ import { initTheme } from '/js/modules/theme.js';
 import '/js/modules/background.js';
 import '/js/modules/boids.js';
 import { initAudio } from '/js/modules/audio.js';
-import { initRouter } from '/js/modules/router.js';
 
 initTheme();
 initAudio();
-initRouter();
